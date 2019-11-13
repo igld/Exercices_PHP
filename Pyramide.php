77d913743622ca913e6c04f7195028f2dbe043de
David <surf_gagnant@hotmail.fr>
2019-11-13 13:03:22 +0100

    Ajout Exo pyramide

--- Pyramide.php
+++ Pyramide.php
@@ -10,16 +10,30 @@ include("common/header.php");
 </form>
 
 <?php
+if(isset($_GET['hauteur']) && $_GET['hauteur'] >=0){
 $hauteur = $_GET['hauteur'];
-echo $hauteur;
+echo "Pour une hauteur de : $hauteur *". "<br> <br>";
 $compteur = $hauteur;
-for($i=0; $i<$hauteur;$i++){
-    
-  
-    
-    
+for($i=0; $i<=$hauteur-1; $i++){
+    AfficheEtoile($i);
 }
 
+//déscente
+for($i=$hauteur-2; $i>=0; $i--){
+    $d=abs($i);
+    AfficheEtoile($d);
+}
+}
+else{
+    echo "<h2>Entrez un nombre d'étoile(s) SVP (un entier positif)</h2>";
+}
+
+function AfficheEtoile($nbreEtoile){
+    for($i=0; $i<=$nbreEtoile; $i++){
+        echo "*";
+    }
+    echo "<br>";
+}
 
 
 ?>
