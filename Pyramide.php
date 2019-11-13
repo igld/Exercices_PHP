ae8637886db98c608270f66a9dcd43d9beac81ec
David <surf_gagnant@hotmail.fr>
2019-11-13 13:17:29 +0100

    Mise en forme Exercices Pyramide

--- Pyramide.php
+++ Pyramide.php
@@ -11,24 +11,25 @@ include("common/header.php");
 
 <?php
 if(isset($_GET['hauteur']) && $_GET['hauteur'] >=0){
-$hauteur = $_GET['hauteur'];
-echo "Pour une hauteur de : $hauteur *". "<br> <br>";
-$compteur = $hauteur;
-for($i=0; $i<=$hauteur-1; $i++){
-    AfficheEtoile($i);
-}
+    $hauteur = $_GET['hauteur'];
+    echo "Pour une hauteur de : $hauteur *". "<br> <br>";
+    $compteur = $hauteur;
+//Monté d'étoiles
+    for($i=0; $i<=$hauteur-1; $i++){
+        AfficheEtoile($i);
+    }
 
-//déscente
-for($i=$hauteur-2; $i>=0; $i--){
-    $d=abs($i);
-    AfficheEtoile($d);
-}
+//descente d'étoiles
+    for($i=$hauteur-2; $i>=0; $i--){
+        $d=abs($i);
+        AfficheEtoile($d);
+    }
 }
 else{
     echo "<h2>Entrez un nombre d'étoile(s) SVP (un entier positif)</h2>";
 }
 
-
+// LES FONCTION(S)
 function AfficheEtoile($nbreEtoile){
     for($i=0; $i<=$nbreEtoile; $i++){
         echo "*";
