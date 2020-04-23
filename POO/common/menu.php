4887dfeac6b45d8487ad65dcfc91eb57b2e43d33
David <surf_gagnant@hotmail.fr>
2020-04-23 13:13:22 +0200

    ajout de menu.php

--- POO/common/menu.php
+++ POO/common/menu.php
@@ -1,9 +1,9 @@
 <body>
-<nav>
-    <ul>
-        <li><a href="index.php">Accueil</a></li>
-        <li><a href="personnage.php">Personnage</a></li>
-        <li><a href="personnageObjet.php">PersonnageObjet</a></li>
+    <nav>
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="personnage.php">Personnage</a></li>
+            <li><a href="personnageObjet.php">PersonnageObjet</a></li>
 
-    </ul>
-</nav>
+        </ul>
+    </nav>
